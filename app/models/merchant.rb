aaf38c7769a9fbf0ceaabf140cd982393cd973db
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-16 15:02:20 -0600

    Adds validates_presence_of

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,5 @@
 class Merchant < ActiveRecord::Base
-validates_presence_of :name, :updated_at
+validates_presence_of :name, :merchant_id, :created_at, :updated_at
 # validates_presence_of :updated_at
 
 
