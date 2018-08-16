c0859c537d40cf08aeec20b9eef64c3b3f4b8749
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-08-16 14:52:17 -0600

    WIP - added validation test on created_at attribute of Merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Merchant do
       expect(merchant).to_not be_valid
 
     end
+    
+    it 'is invalid without a created_at' do
+      merchant = Merchant.create(merchant_id: 2, created_at: nil, updated_at: "never")
+
+      expect(merchant).to_not be_valid
+
+    end
   end
 
 end
